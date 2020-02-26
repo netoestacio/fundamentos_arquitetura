2c4d48d0135a3e6a3114464b83407dac56629fe5
Estacio da Silva Neto <poeo@gft.com>
2020-02-26 09:33:38 -0300

    Add Nodemon, Controller, Router

---
+++ exercicio1/src/routes/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const UsuarioController = require('../controller/UsuarioController')
+
+router.get('/usuario', UsuarioController.index)
+router.post('/usuario', UsuarioController.store)
+router.put('/usuario', UsuarioController.update)
+router.delete('/usuario', UsuarioController.delete)
+
+module.exports = router
